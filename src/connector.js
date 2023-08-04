5444520b2c1ea4c516c1908de7318c205ccc275c
akshaypraf1994 <aks.patil18@gmail.com>
2023-08-04 12:16:37 +0530

    modified3 file ere comited

--- src/connector.js
+++ src/connector.js
@@ -3,10 +3,10 @@ var mysql = require('mysql2');
 
 // Create a MySQL connection using connection parameters
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Akshay@1994",
-    database: "test",
+    host: "sql10.freemysqlhosting.net",
+    user: "sql10637439",
+    password: "qjzNdsAK9p",
+    database: "sql10637439",
     port: 3306 // Port number for the MySQL server
 });
 
